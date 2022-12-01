624690739045911b53222459b00160e95776e8bf
gblajan <george.blajan@8x8.com>
2022-12-01 02:32:29 +0200

    .

--- src/main/java/com/energyconsumptionproducer/producer/ProducerJob.java
+++ src/main/java/com/energyconsumptionproducer/producer/ProducerJob.java
@@ -2,10 +2,10 @@ package com.energyconsumptionproducer.producer;
 
 import org.json.simple.JSONValue;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
 import java.io.*;
 import java.util.HashMap;
@@ -14,7 +14,7 @@ import java.util.Map;
 @Component
 public class ProducerJob {
     private final KafkaTemplate<String, String> kafkaTemplate;
-   // private final BufferedReader csvReader = new BufferedReader(new InputStreamReader(new ClassPathResource("sensor.csv").getInputStream()));
+    private final BufferedReader csvReader = new BufferedReader(new InputStreamReader(ResourceUtils.getURL("classpath:sensor.csv").openStream()));
     @Value("${topic}")
     private String topic;
 
@@ -22,18 +22,17 @@ public class ProducerJob {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 60000)
     public void producerTask() throws IOException {
         String[] array;
-//        String line = csvReader.readLine();
-//        if (line != null && !line.trim().isEmpty()) {
-//            array = line.split(",");
-//            Map<String, String> obj = new HashMap<>();
-//            obj.put("energy", array[0]);
-//            obj.put("time", String.valueOf(System.currentTimeMillis() / 1000));
-//            obj.put("device", array[1]);
-//            kafkaTemplate.send(topic, JSONValue.toJSONString(obj));
-//        }
-        kafkaTemplate.send(topic, "da");
+        String line = csvReader.readLine();
+        if (line != null && !line.trim().isEmpty()) {
+            array = line.split(",");
+            Map<String, String> obj = new HashMap<>();
+            obj.put("energy", array[0]);
+            obj.put("time", String.valueOf(System.currentTimeMillis() / 1000));
+            obj.put("device", array[1]);
+            kafkaTemplate.send(topic, JSONValue.toJSONString(obj));
+        }
     }
 }
