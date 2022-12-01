d529dcbf84be05cf3cf8b872678c527dec758e30
gblajan <george.blajan@8x8.com>
2022-12-01 02:56:13 +0200

    .

--- src/main/java/com/energyconsumptionproducer/producer/ProducerJob.java
+++ src/main/java/com/energyconsumptionproducer/producer/ProducerJob.java
@@ -2,38 +2,37 @@ package com.energyconsumptionproducer.producer;
 
 import org.json.simple.JSONValue;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
-import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class ProducerJob {
     private final KafkaTemplate<String, String> kafkaTemplate;
-    private final BufferedReader csvReader = new BufferedReader(new InputStreamReader(new ClassPathResource("sensor.csv").getInputStream()));
+    private final String[] data = {"3.01,2", " 2,2", "3,2", " 1.45,2", " 1.2,2", "1.7,2", " 3.4,2", "2.2,2", "4.5,2", "6.7,2", "10.9,2"};
+    private int count = 0;
     @Value("${topic}")
     private String topic;
 
-    public ProducerJob(KafkaTemplate<String, String> kafkaTemplate) throws IOException {
+    public ProducerJob(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    @Scheduled(fixedDelay = 60000)
-    public void producerTask() throws IOException {
+    @Scheduled(fixedDelay = 6000)
+    public void producerTask() {
         String[] array;
-        String line = csvReader.readLine();
-        if (line != null && !line.trim().isEmpty()) {
+        if (count < data.length) {
+            String line = data[count];
             array = line.split(",");
             Map<String, String> obj = new HashMap<>();
             obj.put("energy", array[0]);
             obj.put("time", String.valueOf(System.currentTimeMillis() / 1000));
             obj.put("device", array[1]);
             kafkaTemplate.send(topic, JSONValue.toJSONString(obj));
+            count++;
         }
     }
 }
